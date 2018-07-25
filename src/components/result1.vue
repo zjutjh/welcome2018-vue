6889dbd69e5dedaf077b6e565c816454511d9e99
WYuei <392965713@qq.com>
2018-07-25 14:30:46 +0800

    7.25 result1

--- src/components/result1.vue
+++ src/components/result1.vue
@@ -1,5 +1,10 @@
 <template>
-
+  <div class="content">
+    <div class="logo">
+      <img class="logo1" src="../../UI/logo.png">
+    </div>
+    <div>wywwywwyw</div>
+  </div>
 </template>
 
 <script>
@@ -13,5 +18,22 @@
 </script>
 
 <style scoped>
-
+  html{
+    font-size:16px;
+    font-family: 等线;
+  }
+  .content{
+    position: absolute;
+    padding: 0;
+    margin: 0;
+    height: 83.3125rem;
+    width: 46.875rem;
+    background-image: linear-gradient(120deg, #5694dc 0%, #c2e9fb 90%);
+  }
+  .logo1{
+    position: relative;
+    top: 9rem;
+    width:24%;
+    height:24%;
+  }
 </style>
